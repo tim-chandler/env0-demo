d5d3ecc04556c9ff4b0965335c412eb98ef314a1
Tim Chandler <tlc@timchandler.net>
2022-11-14 09:53:11 -0500

    add new RG

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "example" {
   name     = "rg-something-completely-different"
   location = "eastus2"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-same-ole-same-ole"
+  location = "eastus2"
+}
